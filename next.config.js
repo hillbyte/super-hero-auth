248813aa98737a0cdbceb7103cc10f1ee677609a
hillbyte <ahillbaky@gmail.com>
2022-01-12 00:13:20 +0530

    complete

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      "mongodb+srv://prodeskuser:prodeskpass021@prodeskcluster.9gaqm.mongodb.net/next-hero-db?retryWrites=true&w=majority",
-    URL: "https://auth-hero.vercel.app",
-  },
-};
